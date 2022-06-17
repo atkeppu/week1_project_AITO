c2c9a6d215ea1bd15281a65d6beff00eb2a73b69
atkeppu <turpeinen.sami@gmail.com>
2022-06-18 00:00:23 +0300

    Tehty

--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+document.getElementById("add-comment").addEventListener('click', AddUnsortList)
+document.getElementById("remove-comments").addEventListener('click', RemoveAllComments)
+
+
+function AddUnsortList() 
+{  
+  const ul = document.getElementsByTagName("UL")[0];
+  const textare = document.getElementsByTagName("textarea")[0];                   
+  ul.innerHTML += '<li>' +textare.value +' </li>';        
+};
+
+function RemoveAllComments() 
+{  
+  if (confirm("Do you really wants to delete all the comments?") == true) 
+  {  
+    const ul = document.getElementsByTagName("UL")[0];
+    ul.innerHTML = '';
+  } 
+};
+
